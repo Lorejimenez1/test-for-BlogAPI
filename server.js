cb886131cd371afa80bbb09a5da4f2e48fc7eec9
Lorenzo Jimenez <lorenzojimenez101@yahoo.com>
2018-07-02 16:16:39 -0700

    Build CRUD API for bloggin platform

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
+
+const blogRouter = require('./blogRouter');
 const app = express();
 
-app.get('/', (req, res) => res.send('ok'));
+// log the http layer
+app.use(morgan('common'));
+
+app.use(express.json());
+
+//when request come into `/blog-post`
+//we'll route them to express router instances we've imported.
+app.use('/blog-post', blogRouter);
 
 app.listen(process.env.PORT || 8080, () => console.log(
 	`Your app is listening on port ${process.env.PORT || 8080}`)
